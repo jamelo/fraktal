a37b9cca32c292bd4cd1ac63f8e8ef0c2cdc9b81
Jordan Melo <jmelo@uwaterloo.ca>
2014-04-24 00:43:16 -0400

    Initialized project.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
-#include <iostream>
+#include <KApplication>
+#include <KAboutData>
+#include <KCmdLineArgs>
+#include <KUrl>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+int main(int argc, char** argv)
+{
+    KAboutData aboutData(
+        "fractal-viewer",
+        0,
+        ki18n("Fractal Viewer"),
+        "1.0",
+        ki18n("Test program."),
+        KAboutData::License_GPL,
+        ki18n("(c) 2014 Jordan Melo"),
+        ki18n("Enables the exploration and rendering of fractals."),
+        "http://jmelo.net/",
+        "jmelo@uwaterloo.ca"
+    );
+
+    KCmdLineArgs::init(argc, argv, &aboutData);
+
+    KCmdLineOptions options;
+    options.add("+[file]", ki18n("Document to open"));
+    KCmdLineArgs::addCmdLineOptions(options);
+
+    KApplication app;
+
+    //MainWindow* window = new MainWindow();
+    //window->show();
+
+    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+    if (args->count()) {
+        //window->openFile(args->url(0).url());
+    }
+
+    return app.exec();
 }
